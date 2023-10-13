ed5307739eedfa77c898a2fbf0e74033a3d38a64
John Lockwood <john@codesolid.com>
2023-10-13 10:06:42 -0400

    Update buildsite.sh script

--- scripts/buildsite.sh
+++ scripts/buildsite.sh
@@ -1,2 +1,3 @@
-WORKSPACE="/home/runner/work/johnlockwood.github.io/johnlockwood.github.io"
+# WORKSPACE="/home/runner/work/johnlockwood.github.io/johnlockwood.github.io"
+WORKSPACE="/workspaces/johnlockwood.github.io"
 sphinx-build  "${WORKSPACE}/src/" "${WORKSPACE}/docs"
