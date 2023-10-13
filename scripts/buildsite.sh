f9f5ab20556f4940d94e767b18654779bc456e6c
John Lockwood <john@codesolid.com>
2023-10-13 14:15:44 +0000

    Update buildsite.sh -- make portable

--- scripts/buildsite.sh
+++ scripts/buildsite.sh
@@ -1,3 +1,5 @@
-# WORKSPACE="/home/runner/work/johnlockwood.github.io/johnlockwood.github.io"
-WORKSPACE="/workspaces/johnlockwood.github.io"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+WORKSPACE=$(realpath "$SCRIPT_DIR"/..)
+# echo "$SCRIPT_DIR"
+# echo "$WORKSPACE"
 sphinx-build  "${WORKSPACE}/src/" "${WORKSPACE}/docs"
